b62277811ae1e55e7113e2a5dddd1a4f49ddc127
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-02-09 17:40:09 +0000

    Added_case
    Case where wifi completely disconnected.

--- network_change.py
+++ network_change.py
@@ -5,7 +5,10 @@ info = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
 x = [i.split(":")[1] for i in info if "Profile" in i]
 
 for j in x:
-    if 'network_name_1' in str(j.strip()):
+    if x == []:
+        sp.call("netsh wlan connect name=network_name_1")
+
+    elif 'network_name_1' in str(j.strip()):
         sp.call("netsh wlan disconnect")
         sp.call("netsh wlan show networks")
         sp.call("netsh wlan connect name=network_name_2")

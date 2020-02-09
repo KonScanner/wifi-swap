16ef5ebe44ff0d467857733f885ed72d3c4c2a96
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-02-09 22:19:35 +0000

    Added_commands
    Added Sleep timer to ensure connection request has been sent.

--- network_change.py
+++ network_change.py
@@ -1,20 +1,31 @@
 import subprocess as sp
+import time
 
 info = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
     'utf-8', errors="backslashreplace").split('\n')
 x = [i.split(":")[1] for i in info if "Profile" in i]
+sleep = time.sleep(0.15)
 
 for j in x:
     if x == []:
-        sp.call("netsh wlan connect name=network_name_1")
+	sleep
+        sp.call("cmd /c netsh wlan connect name=network_name_1")
+	sleep
 
     elif 'network_name_1' in str(j.strip()):
-        sp.call("netsh wlan disconnect")
-        sp.call("netsh wlan show networks")
-        sp.call("netsh wlan connect name=network_name_2")
+	sleep
+        sp.call("cmd /c netsh wlan disconnect")
+	sleep        
+	sp.call("cmd /c netsh wlan show networks")
+	sleep
+	sp.call("cmd /c netsh wlan connect name=network_name_2")
+	sleep
 
     elif 'network_name_2' in str(j.strip()):
-        sp.call("netsh wlan disconnect")
-        sp.call("netsh wlan show networks")
-        sp.call("netsh wlan connect name=network_name_1")
-
+        sleep
+	sp.call("cmd /c netsh wlan disconnect")
+        sleep
+	sp.call("cmd /c netsh wlan show networks")
+	sleep        
+	sp.call("cmd /c netsh wlan connect name=network_name_1")
+	sleep

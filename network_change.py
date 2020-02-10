4638e555052fff436332f421afd6a981c1c2f393
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-02-10 22:13:27 +0000

    Updated with recursion until connection is successful

--- network_change.py
+++ network_change.py
@@ -1,31 +1,28 @@
 import subprocess as sp
 import time
 
-info = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
+
+data = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
     'utf-8', errors="backslashreplace").split('\n')
-x = [i.split(":")[1] for i in info if "Profile" in i]
+
+x = [i.split(":")[1] for i in data if "Profile" in i]
 sleep = time.sleep(0.15)
+
+
+def connected(channel: str) -> str:
+    connection = sp.check_output(
+        ["cmd", "/c", "netsh", "wlan", "connect", "name={}".format(channel)]).decode('utf-8', errors="backslashreplace").split('\n')
+
+    if 'is not available to connect' in connection:
+        connected(channel)
+
 
 for j in x:
     if x == []:
-	sleep
-        sp.call("cmd /c netsh wlan connect name=network_name_1")
-	sleep
+        connected('Network_Channel_1')
 
-    elif 'network_name_1' in str(j.strip()):
-	sleep
-        sp.call("cmd /c netsh wlan disconnect")
-	sleep        
-	sp.call("cmd /c netsh wlan show networks")
-	sleep
-	sp.call("cmd /c netsh wlan connect name=network_name_2")
-	sleep
+    elif 'Network_Channel_1' in str(j.strip()):
+        connected('Network_Channel_2')
 
-    elif 'network_name_2' in str(j.strip()):
-        sleep
-	sp.call("cmd /c netsh wlan disconnect")
-        sleep
-	sp.call("cmd /c netsh wlan show networks")
-	sleep        
-	sp.call("cmd /c netsh wlan connect name=network_name_1")
-	sleep
+    elif 'Network_Channel_2' in str(j.strip()):
+        connected('Network_Channel_1')

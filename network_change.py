f7256d4e943b36491290891352949397176e0a32
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-06-23 21:16:47 +0100

    update_v_0.3

--- network_change.py
+++ network_change.py
@@ -1,33 +1,46 @@
 import subprocess as sp
 import time
+import platform
 
-data = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
-    'utf-8', errors="backslashreplace").split('\n')
+platform = platform.uname().system
 
-x = [i.split(":")[1] for i in data if "Profile" in i]
-
+# Configurables
 channel_1, channel_2 = 'Network_Channel_1', 'Network_Channel_2'
 
 
-def connected(channel: str) -> str:
-    connection = sp.check_output(
-        ["cmd", "/c", "netsh", "wlan", "connect", "name={}".format(channel)]).decode('utf-8', errors="backslashreplace").split('\n')
+if platform == 'Windows':
+    data = sp.check_output(['netsh', 'wlan', 'show', 'interfaces']).decode(
+        'utf-8', errors="backslashreplace").split('\n')
 
-    if 'is not available to connect' in connection:
-        sp.call("netsh wlan show interfaces")
-        time.sleep(0.1)
-        connected(channel)
+    x = [i.split(":")[1] for i in data if "Profile" in i]
 
-    else:
-        sp.call("cmd /c exit")
+    def connected(channel: str) -> str:
+        connection = sp.check_output(
+            ["cmd", "/c", "netsh", "wlan", "connect", "name={}".format(channel)]).decode('utf-8', errors="backslashreplace").split('\n')
 
+        if 'is not available to connect' in connection:
+            sp.call("netsh wlan show interfaces")
+            time.sleep(0.1)
+            connected(channel)
 
-for j in x:
-    if x == []:
-        connected(channel_1)
+        elif 'request was completed successfully' in connection:
+            print('You are already connected to the network.')
 
-    elif channel_1 in str(j.strip()):
-        connected(channel_2)
+        elif 'There is no profile' in connection:
+            print('The network you are trying to connect to is not in the list of saved network profiles on your machine.')
 
-    elif channel_2 in str(j.strip()):
-        connected(channel_1)
+        else:
+            sp.call("cmd /c exit")
+
+    for j in x:
+        if x == []:
+            connected(channel_1)
+
+        elif channel_1 in str(j.strip()):
+            connected(channel_2)
+
+        elif channel_2 in str(j.strip()):
+            connected(channel_1)
+
+else:
+    print('Linux Implementation coming soon')
